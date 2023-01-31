dbd1ce79f3b33cc08f38f0c69c3c05aa4bc3e0f6
sanjaydharsans <sanjaydharsans@gmail.com>
2023-01-31 12:29:20 +0530

    task

---
+++ words.js
@@ -0,0 +1,35 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "golang",
+        hint: "programming language"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    
+]
